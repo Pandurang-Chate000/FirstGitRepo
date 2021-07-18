8149252f18d458ce011bf3e2860bdaf298d2ff85
Pandurang-Chate000 <87599495+Pandurang-Chate000@users.noreply.github.com>
2021-07-18 14:49:48 +0530

    Update HelloWorldJava.java

--- HelloWorldJava.java
+++ HelloWorldJava.java
@@ -8,4 +8,6 @@ public class HelloWorldJava
 	System.out.println("THis is second line added");
 
 	System.out.println("This change is only in temp branch");
+	
+	System.out.println("This line is edited on github, remote repo in master branch and has to pull on local repo using git pull command");
 }
